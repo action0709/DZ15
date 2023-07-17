80595b25e118634d7d8c9ef35306febe33f465b7
action0709 <action0709@mail.ru>
2023-07-17 17:21:31 +0400

    1

---
+++ src/main/java/org/example/StringListImpl.java
@@ -0,0 +1,107 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class StringListImpl implements StringList {
+    private final String[] storage;
+    private int size;
+
+    public StringListImpl() {
+        storage = new String[10];
+    }
+    public StringListImpl(int inSize) {
+        storage = new String[inSize];
+    }
+
+    @Override
+
+    public String add(String item) {
+        return null;
+    }
+
+    @Override
+    public String add(int index, String item) {
+        return null;
+    }
+
+    @Override
+    public String set(int index, String item) {
+        return null;
+    }
+
+    @Override
+    public String remove(String item) {
+        return null;
+    }
+
+    @Override
+    public String remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(String item) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(String item) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(String item) {
+        return 0;
+    }
+
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean equals(StringList otherList) {
+
+        return Arrays.equals(this.toArray(),otherList.toArray());
+    }
+
+    @Override
+    public int size() {
+
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+    }
+
+    @Override
+    public String[] toArray() {
+           return Arrays.copyOf(storage,size);
+    }
+
+    private void validateItem(String item){
+        if(item == null){
+            throw new NullItemException();
+        }
+    }
+
+    private void validateSize(){
+        if (size == storage.length){
+            throw new StorageFullException();
+                    }
+    }
+
+    private void validateIndex(int Index){
+        if(Index<0||Index>size){
+            throw new InvalidIndexException();
+        }
+    }
+}
